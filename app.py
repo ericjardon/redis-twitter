077936af34ab895706454f2191d5ece3e47ceb82
Eric Chao <ericjardon@hotmail.com>
2021-04-28 14:51:11 -0500

    Menu and app

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 from redis import Redis
-from other import hola
 
-db = Redis(host="localhost", port=6379, db=1)
-
-hola()
-
-db.set("Hello", "World")
-print(db.get("Hello").decode("UTF-8"))
+db = Redis(host="localhost", port=6379, db=0)
 
 # User registration
 def registerUser(user, password):
     return db.hset("users", user, password)
 
-
 def seeUsers():
     return db.hgetall("users")
 
-
 def existUser(user):
-    return db.hexists("users", user)  # regresa bool
+    """Receives: a string of the username
+        Returns: a boolean if it exists as a key in users table"""
+    return db.hexists("users", user)  
 
-
+# User login
 def login(user, password):
+    print("Logging in...", user, password)
+
     if existUser(user):
         print("User exists")
         return password == db.hget("users", user).decode("UTF-8")
@@ -30,23 +26,39 @@ def login(user, password):
     return False
 
 
-# User login
-user = "bryanmon"
-password = "chelas123"
+# Follow a user
+def followUser(user, userToFollow):
+    # the user's channel subscribes to toFollow channel
+    db.sadd(user+":following", userToFollow)
+    db.sadd(userToFollow+":followers", user)
+    
+
+def unfollowUser(user, userToUnfollow):
+    return db.srem(user+":following", userToUnfollow)
+
+# Suscribe to a channel
+def suscribe(user):
+    return db.suscribe(user+":channel")
+
+# Notify a channel the subscripiton
+def notify(user, follower):
+    return db.publish(user+':channel'," started following you")
+
+# Tweet a messages (list)
+def createTweet (user, msg):
+    return db.publish(user+":channel", msg)
+
+
+# See the messages in your timeline
+
 # registerUser(user, password)
-if login(user, password):
-    currentUser = user
-    print("Welcome ", currentUser)
-else:
-    print("Incorrect password or user")
+# if login(user, password):
+#     currentUser = user
+#     print("Welcome ", currentUser)
+# else:
+#     print("Incorrect password or user")
 
 # print(registerUser("bryanmon", "chelas123"))
 
 # solo confirmar si la password ingresada es igual a la existente en redis
 # si sí, todo cool, si no, lanzar excepción o algo a´si
-
-# Follow a new user
-
-# Tweet a messages
-
-# See the messages in your timeline
