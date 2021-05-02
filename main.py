6d0dd668adfb7bc0d9833973082979c76b1d72c5
einar <einarlop0@gmail.com>
2021-05-01 20:02:53 -0500

    Final commit

--- main.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-if __name__ == "__main__":
-    for i in range(int(input("Choose number of clients: "))):
-        subprocess.call("start python3 cli.py", shell=True)
-
-
-input("Press enter to finish base script")
